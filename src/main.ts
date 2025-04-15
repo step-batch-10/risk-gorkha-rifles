223dfdc18379b08bc9249db9072c2dd11f82bb5e
Priyankush <>
2025-04-15 17:40:05 +0530

    deploying the project

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import Server from "./server.ts";
+import Server from './server.ts';
 
 const main = () => {
   const server = new Server();
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { BlankEnv, BlankSchema } from 'hono/types';
+import { serveStatic } from 'hono/deno';
+
 export default class Server {
   start() {
     console.log('Server is started');
+    const app = new Hono();
+    this.appMethod(app);
+
+    Deno.serve({ port: 3000 }, app.fetch);
+  }
+
+  private appMethod(app: Hono<BlankEnv, BlankSchema, '/'>) {
+    app.use(logger());
+    app.get('/', serveStatic({ root: './public' }));
   }
 }
