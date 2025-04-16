8d1d43c936b990d888e01d3a9d0a0c8c3b8ceca0
dinesh_kuniyal <dineshkuniyal0@gmail.com>
2025-04-16 16:28:20 +0530

    #1 | Adds the session model for managing player sessions. Adds the test for session model. | Ankita/Dinesh

---
+++ src/models/session.ts
@@ -0,0 +1,32 @@
+export default class Session {
+  readonly sessions: Map<string, string>;
+  private uniqueId: () => string;
+
+  constructor(uniqueId: () => string = () => "1") {
+    this.uniqueId = uniqueId;
+    this.sessions = new Map();
+  }
+
+  public createSession(userId: string) {
+    const sessionId = this.uniqueId();
+    this.sessions.set(sessionId, userId);
+
+    return sessionId;
+  }
+
+  public findById(sessionId: string) {
+    return this.sessions.get(sessionId);
+  }
+
+  public deleteSession(sessionId: string) {
+    return this.sessions.delete(sessionId);
+  }
+
+  public deleteUsersSession(userId: string) {
+    for (const [key, value] of this.sessions) {
+      if (value === userId) this.deleteSession(key);
+    }
+
+    return this.sessions;
+  }
+};
---
+++ tests/session_test.ts
@@ -0,0 +1,72 @@
+import { assertEquals } from 'assert';
+import { describe, it } from 'testing';
+import Session from '../src/models/session.ts';
+
+describe('tests for session model', () => {
+  it('should create sessions', () => {
+    const session = new Session();
+    const actual = session.createSession("john");
+    const expected = new Map();
+    expected.set('1', 'john');
+
+    assertEquals(actual, "1");
+    assertEquals(session.sessions, expected);
+  });
+
+  it('should delete session', () => {
+    const session = new Session();
+    session.createSession("john");
+    const actual = session.deleteSession("1");
+
+    const expected = new Map();
+
+    assertEquals(actual, true);
+    assertEquals(session.sessions, expected);
+  });
+
+  it('should delete all the sessions of user', () => {
+    const uniqueId = () => {
+      let i = 0;
+
+      return () => (i++).toString();
+    };
+
+    const session = new Session(uniqueId());
+    session.createSession("ankita");
+    session.createSession("john");
+    const sessions = session.deleteUsersSession("ankita");
+
+    const expected = new Map();
+    expected.set('1', 'john');
+
+    assertEquals(sessions, expected);
+  });
+
+  it('should find the user by id', () => {
+    const uniqueId = () => {
+      let i = 0;
+
+      return () => (i++).toString();
+    };
+
+    const session = new Session(uniqueId());
+    session.createSession("ankita");
+    session.createSession("john");
+
+    const actual = session.findById("1");
+    assertEquals(actual, "john");
+  });
+
+  it('should return undefined if session is not valid', () => {
+    const uniqueId = () => {
+      let i = 0;
+
+      return () => (i++).toString();
+    };
+
+    const session = new Session(uniqueId());
+
+    const actual = session.findById("1");
+    assertEquals(actual, undefined);
+  });
+});
