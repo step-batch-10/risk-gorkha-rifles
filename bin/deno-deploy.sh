dd1212fb80bd61ca9e203f6cfb70fe5ba752a458
dinesh_kuniyal <dineshkuniyal0@gmail.com>
2025-04-18 18:41:11 +0530

    [#21] Adds the deno task to deploy on prod and qa | Raj/Shikha

---
+++ bin/deno-deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+SHOULD_POP=0
+
+OUT=$(git stash)
+
+if [ $OUT = "No local changes to save" ]; then
+  SHOULD_POP=1;
+fi
+
+git checkout $2
+echo "deployed"
+# deployctl deploy --project=$1 --entrypoint=src/main.ts --org='Gorkha Rifles' --prod
+git checkout main
+
+if [ $SHOULD_POP -eq 0 ]; then
+git stash pop;
+fi
