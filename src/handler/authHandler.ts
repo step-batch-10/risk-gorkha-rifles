66b342dddb8435bda111fee81ace7c70e69d185e
dinesh_kuniyal <dineshkuniyal0@gmail.com>
2025-06-09 10:43:40 +0530

    Refactored auth handler code

--- src/handler/authHandler.ts
+++ src/handler/authHandler.ts
@@ -3,33 +3,40 @@ import Users from "../models/users.ts";
 import { setCookie } from "hono/cookie";
 import Session from "../models/session.ts";
 
+const isValidUsername = (username: string) => {
+  const usernameRegex = /^[a-zA-Z_][a-zA-Z0-9_]*$/;
+
+  return usernameRegex.test(username);
+};
+
+const invalidUsernameResponse = (context: Context) => {
+  const errorMessage = "Username must contain only letters, numbers, and underscores, and cannot start with a number";
+
+  return context.json({ message: errorMessage }, 400);
+};
+
+const createUserIfNew = (users: Users, username: string, avatar: string) => {
+  if (!users.findIdByUsername(username)) {
+    users.createUser(username, avatar);
+  }
+};
+
 export const loginHandler = async (context: Context) => {
   const { username, avatar } = await context.req.json();
   if (!username)
     return context.json({ message: "Username must be filled out" }, 400);
 
-  const usernameRegex = /^[a-zA-Z_][a-zA-Z0-9_]*$/;
-  if (!usernameRegex.test(username)) {
-    return context.json(
-      {
-        message:
-          "Username must contain only letters, numbers, and underscores, and cannot start with a number",
-      },
-      400
-    );
-  }
+  if (!isValidUsername(username)) return invalidUsernameResponse(context);
 
   const users: Users = context.get("users");
-
-  if (!users.findIdByUsername(username)) {
-    users.createUser(username, avatar);
-  }
+  createUserIfNew(users, username, avatar);
 
   const userId = users.findIdByUsername(username);
-  const sessions: Session = context.get("session");
-  const sessionId = sessions.createSession(userId);
+  const session: Session = context.get("session");
+  const sessionId = session.createSession(userId);
 
   setCookie(context, "sessionId", sessionId);
   setCookie(context, "userId", userId as string);
+
   return context.redirect("/", 302);
 };
