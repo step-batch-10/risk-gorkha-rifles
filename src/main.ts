a481095a08f00668a9ff30013d0cb3f6add223c6
dinesh_kuniyal <dineshkuniyal0@gmail.com>
2025-07-04 13:58:57 +0530

    Resets the server codespace | Rehaul Start

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,8 @@
-import GameManager from "./models/gameManager.ts";
-import Session from "./models/session.ts";
-import Users from "./models/users.ts";
 import Server from "./server.ts";
-import { Continent } from "./types/gameTypes.ts";
-import lodash from "npm:lodash";
-import { getContinents, neighbouringTerritories } from "./utils/continents.ts";
-import Messages from "./models/messages.ts";
-
-export const uniqueId = () => {
-  return Date.now().toString(36) + Math.random().toString(36);
-};
 
 const main = () => {
-  const session = new Session(uniqueId);
-  const users = new Users(uniqueId);
-  const connectedTerritories: Continent = neighbouringTerritories();
-  const messages = new Messages(Date.now, uniqueId);
-  const gameManager = new GameManager(
-    uniqueId,
-    getContinents,
-    Date.now,
-    lodash.shuffle,
-    connectedTerritories,
-    messages
-  );
-
-  const server = new Server(users, session, gameManager, uniqueId);
-  Deno.serve({ port: 3000 }, server.serve());
+  const server = new Server();
+  Deno.serve({ port: 3000 }, server.initialize());
 };
 
 main();
--- src/server.ts
+++ src/server.ts
@@ -1,109 +1,20 @@
-import Users from "./models/users.ts";
 import { logger } from "hono/logger";
 import { serveStatic } from "hono/deno";
-import { Context, Hono, Next } from "hono";
-import Session from "./models/session.ts";
+import { Hono } from "hono";
 import { BlankEnv, BlankSchema } from "hono/types";
-import { loginHandler } from "./handler/authHandler.ts";
-import GameManager from "./models/gameManager.ts";
-import {
-  gameActionsHandler,
-  lobbyStatusHandler,
-  joinGameHandler,
-  profileDetailsHandler,
-  fullProfileDetailsHandler,
-  updateTroopsHandler,
-  requestReinforcementHandler,
-  requestAttackHandler,
-  startGameHandler,
-  cardsHandler,
-  defendingTerritories,
-  getDefendingPlayer,
-  storeTroops,
-  fortificationHandler,
-  connectedTerritoriesHandler,
-  startFortification,
-  saveMessage,
-  getMessages,
-  getGamePlayers,
-} from "./handler/gameHandler.ts";
-import { getCookie } from "hono/cookie";
 
 type App = Hono<BlankEnv, BlankSchema, "/">;
 
 export default class Server {
   private app: App;
-  private users: Users;
-  private session: Session;
-  private gameManager: GameManager;
-  private uniqueId: () => string;
 
-  constructor(
-    users: Users,
-    session: Session,
-    gameManager: GameManager,
-    uniqueId: () => string
-  ) {
+  constructor() {
     this.app = new Hono();
-    this.users = users;
-    this.session = session;
-    this.gameManager = gameManager;
-    this.uniqueId = uniqueId;
   }
 
-  private async setContext(context: Context, next: Next) {
-    context.set("users", this.users);
-    context.set("session", this.session);
-    context.set("uniqueId", this.uniqueId);
-    context.set("gameManager", this.gameManager);
-
-    return await next();
-  }
-
-  private async authHandler(ctx: Context, next: Next) {
-    const sessionId = getCookie(ctx, "sessionId") || "0";
-    const session = ctx.get("session");
-
-    if (sessionId in session.allSessions) {
-      ctx.set("userId", session.findById(sessionId));
-      return await next();
-    }
-
-    return ctx.redirect("/login", 302);
-  }
-
-  private gameHandler() {
-    const app = new Hono();
-    app.use(this.authHandler);
-    app.get("/actions", gameActionsHandler);
-    app.post("/join-game", joinGameHandler);
-    app.get("/lobby-status", lobbyStatusHandler);
-    app.get("/profile-details", profileDetailsHandler);
-    app.get("/player-full-profile", fullProfileDetailsHandler);
-    app.post("/update-troops", updateTroopsHandler);
-    app.post("/request-reinforcement", requestReinforcementHandler);
-    app.get("/request-attack", requestAttackHandler);
-    app.get("/start-game", startGameHandler);
-    app.get("/cards", cardsHandler);
-    app.post("/request-defendTerritories", defendingTerritories);
-    app.post("/request-defendingPlayer", getDefendingPlayer);
-    app.post("/troops-to-attack", storeTroops);
-    app.post("/troops-to-defend", storeTroops);
-    app.post("/fortification", fortificationHandler);
-    app.get("/connected-territories", connectedTerritoriesHandler);
-    app.get("/start-fortification", startFortification);
-    app.post("/messages", saveMessage);
-    app.get("/messages", getMessages);
-    app.get("/players", getGamePlayers);
-    return app;
-  }
-
-  private appMethod(app: App) {
+  private registerRoutes(app: App) {
     app.use(logger());
-    app.use(this.setContext.bind(this));
-    app.post("/login", loginHandler);
-    app.use("/", this.authHandler);
-    app.route("/game", this.gameHandler());
+    app.get("/hello", (c) => c.text("Hello, World!"));
     app.get("*", serveStatic({ root: "./public/" }));
   }
 
@@ -111,8 +22,8 @@ export default class Server {
     return this.app;
   }
 
-  public serve() {
-    this.appMethod(this.app);
+  public initialize() {
+    this.registerRoutes(this.app);
 
     return this.app.fetch;
   }
