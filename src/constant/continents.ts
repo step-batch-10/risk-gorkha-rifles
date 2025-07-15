642c24251f2b393254b502886cecc3565183cf87
dinesh_kuniyal <dineshkuniyal0@gmail.com>
2025-07-15 12:36:47 +0530

    Continents | Update the continents list in the snake-case

--- src/constant/continents.ts
+++ src/constant/continents.ts
@@ -2,77 +2,77 @@ import { Continents } from "../core/territoryManager.ts";
 
 export const getContinents = (): Continents => {
   return {
-    "North America": {
+    NorthAmerica: {
       bonusPoints: 5,
       territories: {
-        "Alaska": { neighbourTerritories: ["Northwest Territory", "Alberta", "Kamchatka"] },
-        "Northwest Territory": { neighbourTerritories: ["Alaska", "Alberta", "Ontario", "Greenland"] },
-        "Greenland": { neighbourTerritories: ["Northwest Territory", "Ontario", "Quebec", "Iceland"] },
-        "Alberta": { neighbourTerritories: ["Alaska", "Northwest Territory", "Ontario", "Western United States"] },
-        "Ontario": { neighbourTerritories: ["Northwest Territory", "Greenland", "Quebec", "Eastern United States", "Western United States", "Alberta"] },
-        "Quebec": { neighbourTerritories: ["Ontario", "Greenland", "Eastern United States"] },
-        "Western United States": { neighbourTerritories: ["Alberta", "Ontario", "Eastern United States", "Central America"] },
-        "Eastern United States": { neighbourTerritories: ["Western United States", "Ontario", "Quebec", "Central America"] },
-        "Central America": { neighbourTerritories: ["Western United States", "Eastern United States", "Venezuela"] }
-      }
+        "alaska": { neighbourTerritories: ["northwest-territory", "alberta", "kamchatka"] },
+        "northwest-territory": { neighbourTerritories: ["alaska", "alberta", "ontario", "greenland"] },
+        "greenland": { neighbourTerritories: ["northwest-territory", "ontario", "quebec", "iceland"] },
+        "alberta": { neighbourTerritories: ["alaska", "northwest-territory", "ontario", "western-united-states"] },
+        "ontario": { neighbourTerritories: ["northwest-territory", "greenland", "quebec", "eastern-united-states", "western-united-states", "alberta"] },
+        "quebec": { neighbourTerritories: ["ontario", "greenland", "eastern-united-states"] },
+        "western-united-states": { neighbourTerritories: ["alberta", "ontario", "eastern-united-states", "central-america"] },
+        "eastern-united-states": { neighbourTerritories: ["western-united-states", "ontario", "quebec", "central-america"] },
+        "central-america": { neighbourTerritories: ["western-united-states", "eastern-united-states", "venezuela"] },
+      },
     },
-    "South America": {
+    SouthAmerica: {
       bonusPoints: 2,
       territories: {
-        "Venezuela": { neighbourTerritories: ["Central America", "Peru", "Brazil"] },
-        "Peru": { neighbourTerritories: ["Venezuela", "Brazil", "Argentina"] },
-        "Brazil": { neighbourTerritories: ["Venezuela", "Peru", "Argentina", "North Africa"] },
-        "Argentina": { neighbourTerritories: ["Peru", "Brazil"] }
-      }
+        "venezuela": { neighbourTerritories: ["central-america", "peru", "brazil"] },
+        "peru": { neighbourTerritories: ["venezuela", "brazil", "argentina"] },
+        "brazil": { neighbourTerritories: ["venezuela", "peru", "argentina", "north-africa"] },
+        "argentina": { neighbourTerritories: ["peru", "brazil"] },
+      },
     },
-    "Europe": {
+    Europe: {
       bonusPoints: 5,
       territories: {
-        "Iceland": { neighbourTerritories: ["Greenland", "Scandinavia", "Great Britain"] },
-        "Scandinavia": { neighbourTerritories: ["Iceland", "Great Britain", "Northern Europe", "Ukraine"] },
-        "Great Britain": { neighbourTerritories: ["Iceland", "Scandinavia", "Northern Europe", "Western Europe"] },
-        "Northern Europe": { neighbourTerritories: ["Scandinavia", "Great Britain", "Western Europe", "Southern Europe", "Ukraine"] },
-        "Western Europe": { neighbourTerritories: ["Great Britain", "Northern Europe", "Southern Europe", "North Africa"] },
-        "Southern Europe": { neighbourTerritories: ["Western Europe", "Northern Europe", "Ukraine", "Middle East", "Egypt", "North Africa"] },
-        "Ukraine": { neighbourTerritories: ["Scandinavia", "Northern Europe", "Southern Europe", "Ural", "Afghanistan", "Middle East"] }
-      }
+        "iceland": { neighbourTerritories: ["greenland", "scandinavia", "great-britain"] },
+        "scandinavia": { neighbourTerritories: ["iceland", "great-britain", "northern-europe", "ukraine"] },
+        "great-britain": { neighbourTerritories: ["iceland", "scandinavia", "northern-europe", "western-europe"] },
+        "northern-europe": { neighbourTerritories: ["scandinavia", "great-britain", "western-europe", "southern-europe", "ukraine"] },
+        "western-europe": { neighbourTerritories: ["great-britain", "northern-europe", "southern-europe", "north-africa"] },
+        "southern-europe": { neighbourTerritories: ["western-europe", "northern-europe", "ukraine", "middle-east", "egypt", "north-africa"] },
+        "ukraine": { neighbourTerritories: ["scandinavia", "northern-europe", "southern-europe", "ural", "afghanistan", "middle-east"] },
+      },
     },
-    "Africa": {
+    Africa: {
       bonusPoints: 3,
       territories: {
-        "North Africa": { neighbourTerritories: ["Brazil", "Western Europe", "Southern Europe", "Egypt", "East Africa", "Congo"] },
-        "Egypt": { neighbourTerritories: ["Southern Europe", "North Africa", "East Africa", "Middle East"] },
-        "East Africa": { neighbourTerritories: ["Egypt", "North Africa", "Congo", "South Africa", "Madagascar", "Middle East"] },
-        "Congo": { neighbourTerritories: ["North Africa", "East Africa", "South Africa"] },
-        "South Africa": { neighbourTerritories: ["Congo", "East Africa", "Madagascar"] },
-        "Madagascar": { neighbourTerritories: ["South Africa", "East Africa"] }
-      }
+        "north-africa": { neighbourTerritories: ["brazil", "western-europe", "southern-europe", "egypt", "east-africa", "congo"] },
+        "egypt": { neighbourTerritories: ["southern-europe", "north-africa", "east-africa", "middle-east"] },
+        "east-africa": { neighbourTerritories: ["egypt", "north-africa", "congo", "south-africa", "madagascar", "middle-east"] },
+        "congo": { neighbourTerritories: ["north-africa", "east-africa", "south-africa"] },
+        "south-africa": { neighbourTerritories: ["congo", "east-africa", "madagascar"] },
+        "madagascar": { neighbourTerritories: ["south-africa", "east-africa"] },
+      },
     },
-    "Asia": {
+    Asia: {
       bonusPoints: 7,
       territories: {
-        "Ural": { neighbourTerritories: ["Ukraine", "Siberia", "Afghanistan", "China"] },
-        "Siberia": { neighbourTerritories: ["Ural", "Yakutsk", "Irkutsk", "Mongolia", "China"] },
-        "Yakutsk": { neighbourTerritories: ["Siberia", "Kamchatka", "Irkutsk"] },
-        "Kamchatka": { neighbourTerritories: ["Yakutsk", "Irkutsk", "Mongolia", "Japan", "Alaska"] },
-        "Irkutsk": { neighbourTerritories: ["Siberia", "Yakutsk", "Kamchatka", "Mongolia"] },
-        "Mongolia": { neighbourTerritories: ["Siberia", "Irkutsk", "Kamchatka", "Japan", "China"] },
-        "Japan": { neighbourTerritories: ["Kamchatka", "Mongolia"] },
-        "Afghanistan": { neighbourTerritories: ["Ural", "China", "India", "Middle East", "Ukraine"] },
-        "China": { neighbourTerritories: ["Afghanistan", "Ural", "Siberia", "Mongolia", "Siam", "India"] },
-        "Middle East": { neighbourTerritories: ["Egypt", "Southern Europe", "Ukraine", "Afghanistan", "India", "East Africa"] },
-        "India": { neighbourTerritories: ["Middle East", "Afghanistan", "China", "Siam"] },
-        "Siam": { neighbourTerritories: ["India", "China", "Indonesia"] }
-      }
+        "ural": { neighbourTerritories: ["ukraine", "siberia", "afghanistan", "china"] },
+        "siberia": { neighbourTerritories: ["ural", "yakutsk", "irkutsk", "mongolia", "china"] },
+        "yakutsk": { neighbourTerritories: ["siberia", "kamchatka", "irkutsk"] },
+        "kamchatka": { neighbourTerritories: ["yakutsk", "irkutsk", "mongolia", "japan", "alaska"] },
+        "irkutsk": { neighbourTerritories: ["siberia", "yakutsk", "kamchatka", "mongolia"] },
+        "mongolia": { neighbourTerritories: ["siberia", "irkutsk", "kamchatka", "japan", "china"] },
+        "japan": { neighbourTerritories: ["kamchatka", "mongolia"] },
+        "afghanistan": { neighbourTerritories: ["ural", "china", "india", "middle-east", "ukraine"] },
+        "china": { neighbourTerritories: ["afghanistan", "ural", "siberia", "mongolia", "siam", "india"] },
+        "middle-east": { neighbourTerritories: ["egypt", "southern-europe", "ukraine", "afghanistan", "india", "east-africa"] },
+        "india": { neighbourTerritories: ["middle-east", "afghanistan", "china", "siam"] },
+        "siam": { neighbourTerritories: ["india", "china", "indonesia"] },
+      },
     },
-    "Australia": {
+    Australia: {
       bonusPoints: 2,
       territories: {
-        "Indonesia": { neighbourTerritories: ["Siam", "New Guinea", "Western Australia"] },
-        "New Guinea": { neighbourTerritories: ["Indonesia", "Western Australia", "Eastern Australia"] },
-        "Western Australia": { neighbourTerritories: ["Indonesia", "New Guinea", "Eastern Australia"] },
-        "Eastern Australia": { neighbourTerritories: ["Western Australia", "New Guinea"] }
-      }
-    }
+        "indonesia": { neighbourTerritories: ["siam", "new-guinea", "western-australia"] },
+        "new-guinea": { neighbourTerritories: ["indonesia", "western-australia", "eastern-australia"] },
+        "western-australia": { neighbourTerritories: ["indonesia", "new-guinea", "eastern-australia"] },
+        "eastern-australia": { neighbourTerritories: ["western-australia", "new-guinea"] },
+      },
+    },
   };
 };
